e7b0f97be89f8bb28bade315c27c8caeb1266ee4
Mayuresh Raul <mraul1@binghamton.edu>
2017-04-07 19:43:13 -0400

    heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule,JsonpModule } from '@angular/http';
+import {RouterModule,Router} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieComponent } from './components/movie/movie.component';
+
+const appRoutes = [
+    {
+        path:'',
+        component: MoviesComponent
+    },
+    {
+        path:'movie/:id',
+        component: MovieComponent
+    }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    JsonpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
